31d07f9176aaf1867e0a494e680be8e24a770efc
Francois DUCATILLON <francois.ducatillon@decathlon.com>
2018-04-10 18:16:38 +0200

    section 6 done

---
+++ UDEMY/06.WEBPACK_VUECLI/cli_project/vue-cli/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Hello {{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  // here it behaves like a vue instance
+  data() {
+    return {
+      msg: "Boss"
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
