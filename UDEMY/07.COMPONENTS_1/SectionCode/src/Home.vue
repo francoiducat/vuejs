ba6338c68e55eaf42c5b4232eea402aa82b4b8ec
Francois DUCATILLON <francois.ducatillon@decathlon.com>
2018-04-13 15:09:51 +0200

    add section 7

---
+++ UDEMY/07.COMPONENTS_1/SectionCode/src/Home.vue
@@ -0,0 +1,24 @@
+<template>
+<p>
+  Server status : {{status}}
+</p>
+<hr>
+<button @click="changeStatus">Change Status</button>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      status: 'Critical'
+    }
+  },
+  methods: {
+    changeStatus() {
+      this.status = 'Normal'
+    }
+  }
+}</script>
+
+<styles></styles>
